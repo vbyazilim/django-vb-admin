c869e09675b5b6ffd802a4314d608d1a23d039df
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2019-08-07 12:35:15 +0300

    Add layout copy

--- vb_admin/command.py
+++ vb_admin/command.py
@@ -1,4 +1,9 @@
+# pylint: disable=R0201
+
 import argparse
+import os
+import shutil
+import sys
 
 __all__ = ['execute_from_command_line']
 
@@ -6,6 +11,25 @@ __all__ = ['execute_from_command_line']
 class Command:
     def __init__(self):
         self.verbosity = 0
+        self.cwd = os.getcwd()
+        self.layout_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'layout')
+        self.target = self.cwd
+
+    def out(self, *messages):
+        sys.stdout.write(f'{" ".join(messages)}\n')
+
+    def err(self, *messages):
+        sys.stderr.write(f'{" ".join(messages)}\n')
+
+    def set_target(self, target_path):
+        if target_path == '.':
+            return
+
+        if not os.path.exists(target_path):
+            self.err(f'Target path not found: {target_path}')
+            sys.exit(1)
+
+        self.target = target_path
 
     def execute(self):
         command_choices = ['startproject']
@@ -14,6 +38,7 @@ class Command:
         parser.add_argument(
             'command', choices=command_choices, type=str, nargs='?', action='store', help='Name of command(s)'
         )
+        parser.add_argument('-t', '--target', type=str, help='Target path', default='.')
         parser.add_argument('-v', '--verbosity', type=int, choices=[0, 1], help='Verbose mode', default=0)
 
         args = parser.parse_args()
@@ -22,12 +47,29 @@ class Command:
             parser.print_help()
 
         self.verbosity = args.verbosity
+        self.set_target(args.target)
 
         if args.command == 'startproject':
             self.startproject()
 
     def startproject(self):
-        pass
+        if self.verbosity > 0:
+            self.out(f'Copying files to: {self.target}')
+
+        for item in os.listdir(self.layout_dir):
+            source = os.path.join(self.layout_dir, item)
+            destination = os.path.join(self.target, item)
+
+            if os.path.isdir(source):
+                shutil.copytree(source, destination)
+            else:
+                shutil.copy2(source, destination)
+
+        if self.verbosity > 0:
+            self.out(f'Copy completed')
+
+        self.out('Now, create your virtual environment and run')
+        self.out('\n\tpip install -r requirements/development.pip\n')
 
 
 def execute_from_command_line():
