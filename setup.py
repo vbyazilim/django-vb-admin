f2d256f77983f0655a5db023e29bd51cf9298c02
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2019-11-27 11:22:25 +0300

    Bump version: 1.0.14 → 1.0.15

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(CURRENT_WORKING_DIRECTORY, 'README.md')) as fp:
 
 setup(
     name='django-vb-admin',
-    version='1.0.14',
+    version='1.0.15',
     description='Create custom Django project with style...',
     long_description=README,
     long_description_content_type='text/markdown',
