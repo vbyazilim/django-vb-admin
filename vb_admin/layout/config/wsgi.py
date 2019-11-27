c8bf83735bb023f67e534a5ddbfca4999f9ab635
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2019-11-27 15:01:39 +0300

    Add CURRENT_DJANGO_VERSION env to context

--- vb_admin/layout/config/wsgi.py
+++ vb_admin/layout/config/wsgi.py
@@ -3,6 +3,7 @@ import subprocess  # noqa: S404
 import sys
 
 from django.core.wsgi import get_wsgi_application
+from django import VERSION
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
@@ -21,7 +22,17 @@ CURRENT_PYTHON_VERSION = (
     f'{sys.version_info.micro}-'
     f'{sys.version_info.releaselevel}'
 )
+
+django_major, django_minor, django_patch, django_tag, __ = VERSION
+CURRENT_DJANGO_VERSION = (
+    f'{django_major}.'
+    f'{django_minor}.'
+    f'{django_patch}-'
+    f'{django_tag}'
+)
+
 os.environ.setdefault('CURRENT_GIT_TAG', CURRENT_GIT_TAG)
 os.environ.setdefault('CURRENT_PYTHON_VERSION', CURRENT_PYTHON_VERSION)
+os.environ.setdefault('CURRENT_DJANGO_VERSION', CURRENT_DJANGO_VERSION)
 
 application = get_wsgi_application()
